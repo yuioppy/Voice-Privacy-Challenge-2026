29f7110e13646d5484e70eaef9851e73b083957c
xiaoxiaomiao323 <152830384+xiaoxiaomiao323@users.noreply.github.com>
2026-02-11 23:11:42 +0800

    Update 02_run.sh

--- 02_run.sh
+++ 02_run.sh
@@ -13,11 +13,9 @@ track=track1 #track1, track2
 if [ -n "$1" ]; then
   anon_config=$1
 else
-  #anon_config=configs/${track}/anon_mcadams.yaml
+  anon_config=configs/${track}/anon_mcadams.yaml
   # anon_config=configs/${track}/anon_sttts.yaml
-  # anon_config=configs/${track}/anon_template.yaml
   # anon_config=configs/${track}/anon_asrbn.yaml
-  anon_config=configs/${track}/anon_ssl.yaml
   # anon_config=configs/${track}/anon_nac.yaml
 fi
 echo "Using config: $anon_config"
@@ -41,11 +39,11 @@ echo python run_anonymization.py --config ${anon_config} ${force_compute}
 # python run_anonymization.py --config ${anon_config} ${force_compute}
 
 # Perform libri dev+test & IEMOCAP dev+test pre evaluation using pretrained ASR/ASV/SER models
-# python run_evaluation.py --config $(dirname ${anon_config})/eval_pre.yaml --overwrite "${eval_overwrite}" ${force_compute}
+# python run_evaluation.py --config $(dirname ${anon_config})/$track1/eval_pre.yaml --overwrite "${eval_overwrite}" ${force_compute}
 
 # # Train post ASV using anonymized libri-360 and perform libri dev+test post evaluation
 # # ASV training takes ~2hours
-python run_evaluation.py --config $(dirname ${anon_config})/eval_post.yaml --overwrite "${eval_overwrite}" ${force_compute}
+python run_evaluation.py --config $(dirname ${anon_config})/$track1/eval_post.yaml --overwrite "${eval_overwrite}" ${force_compute}
 
 # # Merge results
 results_summary_path_orig=$(python3 -c "from hyperpyyaml import load_hyperpyyaml; f = open('$(dirname ${anon_config})/eval_pre.yaml'); print(load_hyperpyyaml(f, ${eval_overwrite}).get('results_summary_path', ''))")
